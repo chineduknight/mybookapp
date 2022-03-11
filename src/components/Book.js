14fb59ec45efdd5f932ac3de94a9b6c7fe5f1333
Chinedu Oguejiofor <chineduknight@gmail.com>
2022-03-11 20:13:43 +0100

    fixed the search error

--- src/components/Book.js
+++ src/components/Book.js
@@ -12,7 +12,7 @@ const Book = (props) => {
           style={{
             width: 128,
             height: 193,
-            backgroundImage: `url(${imageLinks.smallThumbnail})`,
+            backgroundImage: `url(${imageLinks?.smallThumbnail})`,
           }}
         />
         <div className="book-shelf-changer">
